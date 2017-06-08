3e1a77e684aa398215b003eaa87eabe39b6cce4d
SophiemA <sophiemain@163.com>
2017-06-08 10:41:28 +0800

    update

---
+++ demo.js
@@ -0,0 +1,7 @@
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var age = prompt("How old are you ?");
+console.log("your full name is "+ firstName + " "+ lastName);
+console.log("your are "+ age + " years old");
+
+
